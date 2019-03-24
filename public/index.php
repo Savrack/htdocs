bfd06685d5e7834abf624ab5f80b042e268f4ccf
Savrack <47458688+Savrack@users.noreply.github.com>
2019-03-24 18:35:25 +0200

    .htaccess
    Перша спроба переписати цей файл

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
 
 define('DS', DIRECTORY_SEPARATOR);          // втановлення константи DS = '/' чи '\' в залежності від Операційної Системи
-define('ROOT', dirname(dirname(__FILE__))); // втановлення константи ROOT = корневій папці
+define('ROOT', dirname(dirname(dirname(__FILE__))).DS."Mebelka.ua"); // втановлення константи ROOT = корневій папці
 define('VIEWS_PATH', ROOT.DS.'core'.DS.'views');      // втановлення константи для доступу до папки views
-
 
 require_once(ROOT.DS.'core'.DS.'lib'.DS.'init.php');  // підключення автозагрузок класів і конфігурацій(config.php)
 App::run($_SERVER['REQUEST_URI']);
