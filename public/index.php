0cbab2a1af00fb4bd6fefc9e4e3e0c9ee5a54e2b
Savrack <47458688+Savrack@users.noreply.github.com>
2019-03-24 18:43:35 +0200

    Перепис змінних

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DS', DIRECTORY_SEPARATOR);          // втановлення константи DS = '/' чи '\' в залежності від Операційної Системи
-define('ROOT', dirname(dirname(dirname(__FILE__))).DS."Mebelka.ua"); // втановлення константи ROOT = корневій папці
+define('ROOT', dirname(dirname(__FILE__))); // втановлення константи ROOT = корневій папці
 define('VIEWS_PATH', ROOT.DS.'core'.DS.'views');      // втановлення константи для доступу до папки views
 
 require_once(ROOT.DS.'core'.DS.'lib'.DS.'init.php');  // підключення автозагрузок класів і конфігурацій(config.php)
